04013c581341911a286855ced45638e00a9a07b9
Romain Guesdon <r.guesdon@hotmail.fr>
2021-11-26 15:55:47 +0100

    Eval save path

--- model/base_model.py
+++ model/base_model.py
@@ -41,7 +41,7 @@ class BaseModel():
         if self.isTrain:
             self.schedulers = [base_function.get_scheduler(optimizer, opt) for optimizer in self.optimizers]
         if not self.isTrain or opt.continue_train:
-            print('model resumed from %s iteration'%opt.which_iter)
+            print('model resumed from %s iteration' % opt.which_iter)
             self.load_networks(opt.which_iter)
 
     def set_model_to_eval_mode(self):
@@ -56,7 +56,7 @@ class BaseModel():
         for name in self.model_names:
             if isinstance(name, str):
                 net = getattr(self, 'net_' + name)
-                net.train()                  
+                net.train()
 
     def get_image_paths(self):
         """Return image paths that are used to load current data"""
@@ -97,31 +97,31 @@ class BaseModel():
                         visual_ret[name + str(i)] = self.convert2im(value[i], name)
                     # visual_ret[name] = util.tensor2im(value[-1].data)       
                 else:
-                    visual_ret[name] =self.convert2im(value, name)         
-        return visual_ret        
+                    visual_ret[name] = self.convert2im(value, name)
+        return visual_ret
 
     def convert2im(self, value, name):
         if 'label' in name:
             convert = getattr(self, 'label2color')
             value = convert(value)
 
-        if 'flow' in name: # flow_field
+        if 'flow' in name:  # flow_field
             convert = getattr(self, 'flow2color')
             value = convert(value)
 
-        if value.size(1) == 18: # bone_map
-            value = np.transpose(value[0].detach().cpu().numpy(),(1,2,0))
+        if value.size(1) == 18:  # bone_map
+            value = np.transpose(value[0].detach().cpu().numpy(), (1, 2, 0))
             value = pose_utils.draw_pose_from_map(value)[0]
             result = value
 
-        elif value.size(1) == 21: # bone_map + color image
-            value = np.transpose(value[0,-3:,...].detach().cpu().numpy(),(1,2,0))
+        elif value.size(1) == 21:  # bone_map + color image
+            value = np.transpose(value[0, -3:, ...].detach().cpu().numpy(), (1, 2, 0))
             # value = pose_utils.draw_pose_from_map(value)[0]
-            result = value.astype(np.uint8)            
+            result = value.astype(np.uint8)
 
-        elif value.size(1) == 16 and 'flow' not in name: # face map
-            value = np.transpose(value[0,0,...].unsqueeze(0).detach().cpu().numpy(),(1,2,0))
-            result = (value*255).astype(np.uint8)
+        elif value.size(1) == 16 and 'flow' not in name:  # face map
+            value = np.transpose(value[0, 0, ...].unsqueeze(0).detach().cpu().numpy(), (1, 2, 0))
+            result = (value * 255).astype(np.uint8)
 
         else:
             result = util.tensor2im(value.data)
@@ -134,7 +134,7 @@ class BaseModel():
         for i in range(1):
             for j, name in enumerate(self.value_names):
                 if isinstance(name, str):
-                    dis_ret[name+str(i)] =util.tensor2array(value[i][j].data)
+                    dis_ret[name + str(i)] = util.tensor2array(value[i][j].data)
 
         return dis_ret
 
@@ -162,17 +162,19 @@ class BaseModel():
                     net.load_state_dict(torch.load(path))
                     print('load %s from %s' % (name, filename))
                 except FileNotFoundError:
-                    print('do not find checkpoint for network %s'%name)
+                    print('do not find checkpoint for network %s' % name)
                     continue
                 except:
                     pretrained_dict = torch.load(path)
                     model_dict = net.state_dict()
                     try:
-                        pretrained_dict_ = {k:v for k,v in pretrained_dict.items() if k in model_dict}
-                        if len(pretrained_dict_)==0:
-                            pretrained_dict_ = {k.replace('module.',''):v for k,v in pretrained_dict.items() if k.replace('module.','') in model_dict}
-                        if len(pretrained_dict_)==0:
-                            pretrained_dict_ = {('module.'+k):v for k,v in pretrained_dict.items() if 'module.'+k in model_dict}
+                        pretrained_dict_ = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+                        if len(pretrained_dict_) == 0:
+                            pretrained_dict_ = {k.replace('module.', ''): v for k, v in pretrained_dict.items() if
+                                                k.replace('module.', '') in model_dict}
+                        if len(pretrained_dict_) == 0:
+                            pretrained_dict_ = {('module.' + k): v for k, v in pretrained_dict.items() if
+                                                'module.' + k in model_dict}
 
                         pretrained_dict = pretrained_dict_
                         net.load_state_dict(pretrained_dict)
@@ -194,10 +196,10 @@ class BaseModel():
                     net.cuda()
                 if not self.isTrain:
                     net.eval()
-                self.opt.iter_count = util.get_iteration(self.save_dir, filename, name)   
+                self.opt.iter_count = util.get_iteration(self.save_dir, filename, name)
 
+                # def save_results(self, save_data, score=None, data_name='none'):
 
-    # def save_results(self, save_data, score=None, data_name='none'):
     #     """Save the training or testing results to disk"""
     #     img_paths = self.get_image_paths()
 
@@ -219,8 +221,6 @@ class BaseModel():
     #         img_numpy = util.tensor2im(save_data[i].data)
     #         util.save_image(img_numpy, img_path)
 
-
-
     def save_results(self, save_data, data_name='none', data_ext='jpg'):
         """Save the training or testing results to disk"""
         img_paths = self.get_image_paths()
@@ -231,10 +231,11 @@ class BaseModel():
             name = os.path.splitext(short_path)[0]
             img_name = '%s_%s.%s' % (name, data_name, data_ext)
 
-            util.mkdir(self.opt.results_dir)
-            img_path = os.path.join(self.opt.results_dir, img_name)
+            results_dir = os.path.join(self.opt.results_dir, os.path.basename(os.path.normpath(self.opt.dataroot)))
+            util.mkdir(results_dir)
+            img_path = os.path.join(results_dir, img_name)
             img_numpy = util.tensor2im(save_data[i].data)
-            util.save_image(img_numpy, img_path)            
+            util.save_image(img_numpy, img_path)
 
     def save_feature_map(self, feature_map, save_path, name, add):
         if feature_map.dim() == 4:
@@ -242,12 +243,12 @@ class BaseModel():
         name = name[0]
         feature_map = feature_map.detach().cpu().numpy()
         for i in range(feature_map.shape[0]):
-            img = feature_map[i,:,:]
+            img = feature_map[i, :, :]
             img = self.motify_outlier(img)
-            name_ = os.path.join(save_path, os.path.splitext(os.path.basename(name))[0], add+'modify'+str(i)+'.png')
+            name_ = os.path.join(save_path, os.path.splitext(os.path.basename(name))[0],
+                                 add + 'modify' + str(i) + '.png')
             util.mkdir(os.path.dirname(name_))
             plt.imsave(name_, img, cmap='viridis')
-
 
     def motify_outlier(self, points, thresh=3.5):
 
@@ -258,10 +259,8 @@ class BaseModel():
         modified_z_score = 0.6745 * diff / med_abs_deviation
 
         outliers = points[modified_z_score > thresh]
-        outliers[outliers>median] = thresh*med_abs_deviation/0.6745 + median
-        outliers[outliers<median] = -1*(thresh*med_abs_deviation/0.6745 + median)
-        points[modified_z_score > thresh]=outliers
+        outliers[outliers > median] = thresh * med_abs_deviation / 0.6745 + median
+        outliers[outliers < median] = -1 * (thresh * med_abs_deviation / 0.6745 + median)
+        points[modified_z_score > thresh] = outliers
 
         return points
-
-        
